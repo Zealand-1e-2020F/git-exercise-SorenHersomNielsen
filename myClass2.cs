92f91f68e20dd63a7dafc0d7cec83567641c1408
SorenHersomNielsen <hersom@ellekrattet.dk>
2020-04-01 13:09:08 +0200

    Create myClass2.cs
    Jeg har lavet en ny class

---
+++ myClass2.cs
@@ -0,0 +1,23 @@
+namespace GitExercise
+{
+    class myClass2
+    {
+        public string Navn {get; set;}
+        public string Adresse {get; set;}
+        public int Postnr {get; set;}
+        public string By {get; set;}
+
+        public myClass2 (string navn, string adresse, int postnr, string by)
+        {
+            Navn = navn;
+            Adresse = adresse;
+            Postnr = postnr;
+            By = by;
+        }
+        public override string ToString()
+        {
+            return $"Person hedder {Navn}, personen bor {Adresse} i postnr {Postnr} og bor i byen {By}";
+        }
+        
+    }
+}
